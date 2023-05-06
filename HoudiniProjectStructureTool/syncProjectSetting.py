37b813095ed8080c23a485d0dbb252178deb99c1
weishc <61315168+weishc@users.noreply.github.com>
2023-05-06 16:57:56 -0400

    Delete HoudiniProjectStructureTool directory

--- HoudiniProjectStructureTool/syncProjectSetting.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-url = "http://maps.googleapis.com/maps/api/geocode/json?address=googleplex&sensor=false"
-#proxy = {'http': 'http://okapi.tnn:14240', 'https': 'https://okapi.tnn:14240'}
-result = requests.get(url, proxies=proxy)
-data = result.json()
-
-print (data['status'])
