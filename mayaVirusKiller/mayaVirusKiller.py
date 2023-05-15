86f703096671ddf3bf17269661db0f0100c1e7f7
weishc <61315168+weishc@users.noreply.github.com>
2023-05-15 11:28:45 -0400

    Create mayaVirusKiller.py

---
+++ mayaVirusKiller/mayaVirusKiller.py
@@ -0,0 +1,42 @@
+import os
+
+
+def main(fpath):
+    word = 'createNode script -n'
+    del_list = []
+    with open(fpath, 'r') as fp:
+        lines = fp.readlines()
+        found = 0
+        for idx, line in enumerate(lines):
+            if line.startswith(word) and '_gene' in line:
+                found = 1
+                del_list.append(idx)
+                continue
+            if found > 0 and line.startswith('	'):
+                del_list.append(idx)
+            if found > 0 and not line.startswith('	'):
+                found -= 1
+    for i in del_list:
+        lines[i] = ''
+    with open(fpath, 'w') as fp:
+        fp.writelines(lines)
+
+
+def fix_ma_with_subfolder(path):
+    for r, d, f in os.walk(path):
+        for i in f:
+            if i[-3:] == '.ma':
+                fpath = os.path.join(r, i)
+                main(fpath)
+
+
+def fix_ma(path):
+    for i in os.listdir(path):
+        if i[-3:] == '.ma':
+            fpath = os.path.join(path, i)
+            main(fpath)
+
+
+path = r'I:\MXA\test'
+fix_ma(path)
+fix_ma_with_subfolder(path)
